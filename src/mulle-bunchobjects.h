7964b5d1cb3c64367849d5ee3ffa1d1c552a90fa
Nat! <nat@mulle-kybernetik.com>
2022-06-23 20:10:31 +0200

    modernized a little

--- src/mulle-bunchobjects.h
+++ src/mulle-bunchobjects.h
@@ -51,7 +51,7 @@
 //
 #ifdef USE_BUNCH_OBJECTS
 
-#define MULLE_BUNCHOBJECTS_VERSION   ((0 << 20) | (20 << 8) | 0)
+#define MULLE_BUNCHOBJECTS_VERSION   ((0 << 20) | (20 << 8) | 1)
 
 #include <mulle-thread/mulle-thread.h>
 #include <stdint.h>
