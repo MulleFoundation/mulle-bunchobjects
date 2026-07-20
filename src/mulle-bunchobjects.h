ab23b739312ed4acdd7f3ef58e0647fd1aea88be
Nat! <nat@mulle-kybernetik.com>
2026-07-20 17:00:01 +0200

    CI settings, extension freshening, and prerelease setup

--- src/mulle-bunchobjects.h
+++ src/mulle-bunchobjects.h
@@ -51,7 +51,7 @@
 //
 #ifdef USE_BUNCH_OBJECTS
 
-#define MULLE_BUNCHOBJECTS_VERSION   ((0UL << 20) | (20 << 8) | 8)
+#define MULLE_BUNCHOBJECTS_VERSION   ((0UL << 20) | (20 << 8) | 9)
 
 #include <mulle-thread/mulle-thread.h>
 #include <stdint.h>
